5b4930f677fe9dbf3a847d66dfe51593fe0395d5
Tersoo <tersooahire21@gmail.com>
2023-04-29 08:05:23 +0100

    added technlogies used with icons

--- project/src/components/Project.jsx
+++ project/src/components/Project.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import LALMButtons from "../components/LiveApp-LearnMore";
-
+import html from "../assets/html.svg";
+import css from "../assets/css.svg";
+import javascript from "../assets/javascript.svg";
+import git from "../assets/git.svg";
+import django from "../assets/django.svg";
+import python from "../assets/python.svg";
+import pythonanywhere from "../assets/pythonanywhere.svg";
+import vite from "../assets/vite.svg";
+import react from "../assets/react.svg";
+import node from "../assets/nodejs.svg";
 
 export default function Project(){
     return (
@@ -15,6 +24,16 @@ export default function Project(){
                 </p>
                 <p className="project-p3">
                     Technologies used: 
+                    <img src={html} alt="" />
+                    <img src={css} alt="" />
+                    <img src={javascript} alt="" />
+                    <img src={git} alt="" />
+                    <img src={django} alt="" />
+                    <img src={python} alt="" />
+                    <img src={pythonanywhere} alt="" />
+                    <img src={vite} alt="" />
+                    <img src={react} alt="" />
+                    <img src={node} alt="" />
                 </p>
                 <LALMButtons/>
             </div>
