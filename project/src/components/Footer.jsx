1860f44c9e45cd20a67929e54038242d20875cf7
Tersoo <tersooahire21@gmail.com>
2023-05-09 08:57:03 +0100

    created mobile responsive footer

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import '../styles/footer.css'
+import '../styles/footer.css';
+import '../styles_mobile/footer.css';
 import { Link } from "react-router-dom";
 import logo from "../assets/tersoo-logo.svg";
 import github from "../assets/github.svg";
@@ -11,9 +12,13 @@ export default function Footer(){
     return (
         <footer>
             <div>
-              <Link to="/"> <img src={logo} alt="Tersoo Logo" width="150px" /></Link>
+              <Link to="/"> <img src={logo} alt="Tersoo Logo" width="150px" className="image" /></Link>
             </div>
-            <p>© 2022 Freelancer - Ahire Tersoo David. All rights reserved.</p>
+            
+            <em className="email">tersooahire21@gmail.com</em>
+            
+            <p className="freelance1">© 2022 Freelancer - Ahire Tersoo David. All rights reserved.</p>
+            
             <div className="socials-container">
                 <a href="https://github.com/tersoo-ahire" target="_blank" rel="noopener noreferrer" className="social-icon">
                     <img src={github} alt="Github Icon"/>
@@ -28,6 +33,16 @@ export default function Footer(){
                     <img src={behance} alt="Behance Icon"/>
                 </a>
             </div>
+            
+            <p className="title">QUICK LINKS</p>
+
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+
+            <p className="freelance2">© 2022 Freelancer - Ahire Tersoo David. All rights reserved.</p>
         </footer>
     )
 }
