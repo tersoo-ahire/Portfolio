d73f31a8332d7c7035e2b5b07e535ae7781bf49f
Ahire Tersoo David <tersooahire21@gmail.com>
2023-09-11 04:27:31 +0100

    added new project to portfolio

--- project/src/data/project.js
+++ project/src/data/project.js
@@ -1,7 +1,7 @@
 import chunkfile from "../assets/chunkfile_hero.png";
-import asadag from "../assets/asadag_hero.png";
-import portfolio from "../assets/portfolio_hero.png";
 import britishauc from "../assets/britishauc_hero.png";
+import moviefinder from "../assets/moviefinder_hero.png";
+import coonected from "../assets/coonected_hero.png";
 import html from "../assets/html.svg";
 import css from "../assets/css.svg";
 import javascript from "../assets/javascript.svg";
@@ -9,61 +9,61 @@ import python from "../assets/python.svg";
 import django from "../assets/django.svg";
 import git from "../assets/git.svg";
 import figma from "../assets/figma.svg";
-import bootstrap from "../assets/bootstrap.svg";
 import react from "../assets/react.svg";
 import vite from "../assets/vite.svg";
 import scss from "../assets/scss.svg";
 import nodejs from "../assets/nodejs.svg";
+import csharp from "../assets/csharp.svg";
+import dotnetcore from "../assets/dotnetcore.svg";
 
 export default [
-    {
-        id: 1,
-        project_name: "British AUC",
-        project_description: 
-        `Corporate website built using React for British American University Consulting. This site is fully responsive across all screens.`,
-        technology: [react, html, css, scss, javascript, nodejs, vite, git, figma],
-        display1: "display1",
-        display2: "display2",
-        live_link: "https://www.britishauc.com/",
-        figma_link: "https://www.figma.com/file/0X2dvIeQisVcYjP51z5Fbv/Website?type=design&node-id=0%3A1&mode=design&t=Y3PqZHK9cLXZn65r-1",
-        project_image: britishauc,
-    },
-    {
-        id: 2,
-        project_name: "Africa Students Association in Russia Website",
-        project_description: 
-        `Corporate website built using Bootstrap for the African Students Association in Russia. This site is fully responsive across all screens.`,
-        technology: [html, css, javascript, bootstrap, git, figma],
-        display1: "display2",
-        display2: "display1",
-        live_link: "https://www.asadag.com",
-        figma_link: "https://www.figma.com/file/zJRW79owSAlznMtUg4IogV/My-Projects?type=design&node-id=526-1318",
-        project_image: asadag,
-    },
-    {
-        id: 3,
-        project_name: "Portfolio Site",
-        project_description: 
-        `My portfolio site is a showcase of my work and skills as a web developer and designer. It features a collection of projects that highlight my expertise in various technologies and design tools. 
-        \n The site itself is built using the React library for dynamic interactivity. In the contact form, I integrated Axios for handling API requests and EmailJS for streamlined message delivery.`,
-        technology: [react, html, css, javascript, vite, git, figma],
-        display1: "display1",
-        display2: "display2",
-        live_link: "/",
-        figma_link: "https://www.figma.com/file/zJRW79owSAlznMtUg4IogV/My-Projects?type=design&node-id=355%3A355&t=544sPtexefvBX6nC-1",
-        project_image: portfolio,
-    },
-    {
-        id: 4,
-        project_name: "ChunkFile",
-        project_description: 
-        `ChunkFile is a service that allows individuals to upload large CSV or JSON files and split the dataset into multiple files.
+  {
+    id: 1,
+    project_name: "Movie Finder",
+    project_description: `Movie Finder is a web application that allows users to search for movies, access detailed information, and watch trailers. \n \n Built with React, .NET Core, and OMDB's Rest API, it offers an engaging user interface. Real-time updates ensure users have access to the latest information, including release dates and ratings.`,
+    technology: [react, dotnetcore, html, css, scss, javascript, csharp, vite, git, figma],
+    display1: "display1",
+    display2: "display2",
+    live_link: "https://tersoomoviefinderproject.netlify.app/",
+    figma_link:
+      "https://github.com/tersoo-ahire/Movie-Search-App",
+    project_image: moviefinder,
+  },
+  {
+    id: 2,
+    project_name: "British AUC",
+    project_description: `Corporate website built using React and Nodejs for British American University Consulting. This site is fully responsive across all screens.`,
+    technology: [react, html, css, scss, javascript, nodejs, vite, git, figma],
+    display1: "display2",
+    display2: "display1",
+    live_link: "https://www.britishauc.com/",
+    figma_link:
+      "https://www.figma.com/file/0X2dvIeQisVcYjP51z5Fbv/Website?type=design&node-id=0%3A1&mode=design&t=Y3PqZHK9cLXZn65r-1",
+    project_image: britishauc,
+  },
+  {
+    id: 3,
+    project_name: "Coonected (Coming Soon)",
+    project_description: `Coonected is a social networking platform designed to foster meaningful connections. \n \n It utilizes React for its responsive frontend, providing users with an intuitive interface. On the backend, Python and Django Rest APIs power its robust infrastructure, enabling secure data management, real-time messaging, and personalized matching.`,
+    technology: [react, html, css, scss, javascript, python, django, vite, git, figma],
+    display1: "display1",
+    display2: "display2",
+    live_link: "/",
+    figma_link:
+      "/",
+    project_image: coonected,
+  },
+  {
+    id: 4,
+    project_name: "ChunkFile",
+    project_description: `ChunkFile is a service that allows individuals to upload large CSV or JSON files and split the dataset into multiple files.
         \n The platform utilizes Python and Django for the backend authentication, ensuring secure access for users. SQLite is used as the database, allowing efficient storage and retrieval of data.`,
-        technology: [html, css, javascript, python, django, git, figma],
-        display1: "display2",
-        display2: "display2",
-        live_link: "https://chunkfile.pythonanywhere.com/",
-        figma_link: "https://www.figma.com/file/sm2Io642yiuO5fjg2cmXhT/ChunkFile?type=design&node-id=2305%3A11431&t=SIfzxLrAZD01uoS3-1",
-        project_image: chunkfile,
-    },
-]
+    technology: [html, css, javascript, python, django, git, figma],
+    display1: "display2",
+    display2: "display2",
+    live_link: "https://chunkfile.pythonanywhere.com/",
+    figma_link:
+      "https://www.figma.com/file/sm2Io642yiuO5fjg2cmXhT/ChunkFile?type=design&node-id=2305%3A11431&t=SIfzxLrAZD01uoS3-1",
+    project_image: chunkfile,
+  },
+];
